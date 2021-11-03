16e45358106f512ca29cdb3ecddf662d01c41d85
lakshyapriyadarshi.me <lakshyapriyadarshime@gmail.com>
2021-11-04 00:29:18 +0530

    Adding content

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,4 +1,4 @@
-    function myFunction() {
+    function myFunction_responsive() {
       var x = document.getElementById("myTopnav");
       if (x.className === "topnav") {
         x.className += " responsive";
